014ed81c727e29092ad819402e4cc9449e248860
Tim Fleming <tfleming26@hotmail.com>
2018-06-12 09:18:25 -0600

    Add a function to get user by id or a custom attribute

--- lib/rumblify/repo.ex
+++ lib/rumblify/repo.ex
@@ -7,4 +7,14 @@ defmodule Rumblify.Repo do
      %Rumblify.User{id: "2", name: "Dash", username: "dmcflash", password: "isitmorning"},
      %Rumblify.User{id: "3", name: "Zippy", username: "zippymcnutty", password: "mmmyummy"}]
   end
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
 end
