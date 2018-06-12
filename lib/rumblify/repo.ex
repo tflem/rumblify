af841f48566b6458c60f0d5c6948763adca11164
Tim Fleming <tfleming26@hotmail.com>
2018-06-12 09:08:57 -0600

    Add in-memory repository of users

--- lib/rumblify/repo.ex
+++ lib/rumblify/repo.ex
@@ -1,3 +1,10 @@
 defmodule Rumblify.Repo do
-  use Ecto.Repo, otp_app: :rumblify
+  @moduledoc """
+  In memory repository
+  """
+  def all(Rumblify.User) do
+    [%Rumblify.User{id: "1", name: "Patrick", username: "pstar", password: "whatdidimiss"},
+     %Rumblify.User{id: "2", name: "Dash", username: "dmcflash", password: "isitmorning"},
+     %Rumblify.User{id: "3", name: "Zippy", username: "zippymcnutty", password: "mmmyummy"}]
+  end
 end
