8aa72293315600696705c422331cb07c45e0e604
Tim Fleming <tfleming26@hotmail.com>
2018-06-18 03:19:39 -0600

    Add user show action and template

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -5,4 +5,9 @@ defmodule Rumblify.UserController do
     users = Repo.all(Rumblify.User)
     render conn, "index.html", users: users
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Rumblify.User, id)
+    render conn, "show.html", user: user
+  end
 end
